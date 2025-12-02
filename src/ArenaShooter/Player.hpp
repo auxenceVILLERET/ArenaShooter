d09a594a64986cc8679cc01a97a7b8e8dc336f86
Vivien Singier <vsingier@gaming.tech>
2025-12-02 15:57:20 +0100

    [ADD] Health Script

---
+++ src/ArenaShooter/Player.hpp
@@ -0,0 +1,23 @@
+#ifndef PLAYER_HPP_INCLUDED
+#define PLAYER_HPP_INCLUDED
+
+#include "define.h"
+#include "Script.h"
+#include "GameObject.h"
+
+DECLARE_SCRIPT(PlayerMovement, ScriptFlag::Start | ScriptFlag::Update)
+
+void Start() override
+{
+	
+}
+
+
+void Update() override
+{
+
+}
+
+END_SCRIPT
+
+#endif // !PLAYER_HPP_DEFINED
