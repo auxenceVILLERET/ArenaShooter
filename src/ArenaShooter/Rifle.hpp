498cbd0637f0232bbd78fa03c57ab2577decfb59
Hexor <avilleret@gaming.tech>
2025-12-03 13:15:04 +0100

    [REF] debut rifle

---
+++ src/ArenaShooter/Rifle.hpp
@@ -0,0 +1,30 @@
+#ifndef RIFLE_HPP_INCLUDED
+#define RIFLE_HPP_INCLUDED
+
+#include "define.h"
+#include "Script.h"
+#include "GameObject.h"
+#include "Chrono.h"
+#include "Weapon.hpp"
+
+
+DECLARE_CHILD_SCRIPT(Rifle, Weapon, ScriptFlag::Start | ScriptFlag::Update)
+
+void Start() override
+{
+    m_maxAmmo = 25;
+    m_ammo = 25;
+    m_shotCooldown = 0.1f;
+    m_reloadCooldown = 1.5f;
+}
+
+void Shoot() override
+{
+    m_ammo--;
+    m_shotTimer.Start();
+    std::cout << m_ammo << std::endl;
+}
+
+END_SCRIPT
+
+#endif // !RIFLE_HPP_INCLUDED
--- src/ArenaShooter/Weapon.hpp
+++ src/ArenaShooter/Weapon.hpp
@@ -11,51 +11,80 @@ DECLARE_SCRIPT(Weapon, ScriptFlag::Start | ScriptFlag::Update)
 int8 m_ammo = 0;
 int8 m_maxAmmo = 0;
 
-float32 m_reloadCooldown = 0.f; 
+float32 m_reloadCooldown = 0.f;
 float32 m_shotCooldown = 0.f;
 
 gce::Chrono m_reloadTimer;
 gce::Chrono m_shotTimer;
 
-bool m_isShooting = false; 
+bool m_isShooting = false;
 bool m_isReloading = false;
+
 
 void Start() override
 {
-
+    
 }
 
 void Update() override
 {
+    if (m_ammo <= 0)
+    {
+        m_isReloading = true;
+    }
+
+    if (m_isReloading == true)
+    {
+        if (m_reloadTimer.GetElapsedTime() >= m_reloadCooldown)
+        {
+            m_ammo = m_maxAmmo;
+            m_isReloading = false;
+            m_reloadTimer.Pause();
+            m_reloadTimer.Reset();
+        }
+        return;
+    }
+
+    
+    if (m_shotTimer.GetElapsedTime() < m_shotCooldown)
+        return;
+
+
+    if (m_isShooting)
+        Shoot();
+
 
 }
+
 
 virtual void BeginShot()
 {
-	if (m_isReloading == true) return;
-	if (m_isShooting == true) return;
+    if (m_isReloading) return;
+    if (m_ammo <= 0) return;
 
-	m_isShooting = true;
+    m_isShooting = true;
 }
 
-template<typename T>
-
-void Shoot() 
+virtual void Shoot()
 {
-	
-
+    
 }
 
 virtual void EndShot()
 {
-
+    m_isShooting = false;
+    m_shotTimer.Pause();
+    m_shotTimer.Reset();
 }
 
 void Reload()
 {
-	m_ammo = m_maxAmmo;
+    if (m_isReloading) return;
+
+    m_isReloading = true;
+    m_reloadTimer.Start();
 }
 
 END_SCRIPT
 
-#endif // ! WEAPON_HPP_DEFINED
+#endif // !WEAPON_HPP_INCLUDED
