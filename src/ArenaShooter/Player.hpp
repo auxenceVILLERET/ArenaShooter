d73b5c99ff2337c39f653a80f1a1a5f3ec6d52b3
Vivien Singier <vsingier@gaming.tech>
2025-12-04 10:51:57 +0100

    [ADD] Linked camera to player

--- src/ArenaShooter/Player.hpp
+++ src/ArenaShooter/Player.hpp
@@ -18,11 +18,22 @@ float32 m_deltaTime;
 
 void Start() override
 {
-	m_pOwner->transform.SetWorldPosition({ 0,1,0 });
-	m_pOwner->transform.SetWorldScale({ 1,1,1 });
+	m_pOwner->transform.SetWorldPosition({ 0,2,0 });
+	m_pOwner->transform.SetWorldScale({ 0.8f,1.7f,0.8f });
 	m_pOwner->AddComponent<BoxCollider>();
 	m_pOwner->AddComponent<PhysicComponent>();
-
+	
+	GameObject& cam = GameObject::Create(m_pOwner->GetScene());
+	Camera& camera = *cam.AddComponent<Camera>();
+	camera.SetMainCamera();
+	camera.SetType(PERSPECTIVE);
+	camera.perspective.fov = XM_PIDIV4;
+	camera.perspective.nearPlane = 0.001f;
+	camera.perspective.farPlane = 500.0f;
+	camera.perspective.aspectRatio = 600.0f / 400.0f;
+	camera.perspective.up = { 0.0f, 1.0f, 0.0f };
+	cam.SetParent(*m_pOwner);
+	cam.transform.SetLocalPosition({0.f, 0.8f, 0.f});
 }
 
 
